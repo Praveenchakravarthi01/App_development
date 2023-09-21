0871378b72c2ca54f6c55ca98e6b8566d4704e6b
Praveen chakravarthi S <110585276+chakravarthi1504@users.noreply.github.com>
2023-09-21 14:45:03 +0530

    DAY 4

---
+++ DAY 4/src/pages/Admin/Organization.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../redux/userSlice'
+import Topbar from '../../components/Topbar'
+import Leftbar from '../../components/Leftbar'
+const Organization = () => {
+  
+  return (
+    <>
+      <Leftbar />
+      <Topbar title='Organization'/>
+      <div className="pt-20 z-50 pl-80">
+        <div className="flex-col pt-[70px] items-center">
+            <span className='text-black text-4xl pl-[80px]'>No organization available</span>
+            <img className='w-1/2' src="https://thumbs.dreamstime.com/z/documentation-paper-earth-icon-element-no-government-organisation-white-background-166294266.jpg?w=768" alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Organization
+
